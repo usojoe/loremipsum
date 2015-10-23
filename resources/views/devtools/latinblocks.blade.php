436b1bfb69830146996a0eb9548985d1577e86f7
Joe Lee <josephandrewlee@g.harvard.edu>
2015-10-22 21:23:37 -0400

    updating

--- resources/views/devtools/latinblocks.blade.php
+++ resources/views/devtools/latinblocks.blade.php
@@ -2,6 +2,10 @@
 
 @section('displaylorem')
 
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
 {{ $blocks }}
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @stop
